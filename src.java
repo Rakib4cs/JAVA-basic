8ab26d50cef79dba2e4f1ad31bfc3863c2db0708
Rakib Hasan Riyad <rkbhsnriyad@gmail.com>
2026-01-14 23:40:38 +0600

    Add files via upload
    Variable Types

---
+++ src.java
@@ -0,0 +1,25 @@
+
+public class Variable{
+    public static void main(String[]args){
+
+        int iv =13;
+        double d= 8;
+        float a= 1.5f;
+        boolean b= true;
+        short s= 1000;
+        char c = 'K';
+        long l= 061224205101L;
+        byte z=10;
+
+        System.out.println(iv);
+        System.out.println(d);
+        System.out.println("a:"+a);
+        System.out.println(b);
+        System.out.println("s:\n"+s);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println("z:\t"+z);
+
+
+    }
+}
